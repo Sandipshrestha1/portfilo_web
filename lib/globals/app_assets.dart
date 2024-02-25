e02dbc8b1eb5d0f8ca1c1d8433a0dce8be0964dc
Sandipshrestha1 <sandipshrestha2050@gmail.com>
2024-02-25 23:06:26 +0545

    updated to ui of service page

--- lib/globals/app_assets.dart
+++ lib/globals/app_assets.dart
@@ -2,8 +2,10 @@ class AppAssets {
   static String facebook = 'assets/images/face.png';
   static String github = 'assets/images/git.png';
   static String linkedIn = 'assets/images/in.png';
-  static String insta = 'assets/images/insta.png';
+  static String insta =   'assets/images/insta.png';
   static String twitter = 'assets/images/twit.png';
   static String profile = 'assets/images/pp.jpeg';
-  static String  code = 'assets/images/coding.png';
+  static String  code =   'assets/images/coding.png';
+  static String brush =   'assets/images/brush-stroke.png';
+  static String analyst = 'assets/images/analytics.png';
 }
--- lib/views/my_services.dart
+++ lib/views/my_services.dart
@@ -1,6 +1,7 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:portfolio_app/globals/app_assets.dart';
+import 'package:portfolio_app/globals/app_buttons.dart';
 import 'package:portfolio_app/globals/app_colors.dart';
 import 'package:portfolio_app/globals/app_text_styles.dart';
 import 'package:portfolio_app/globals/constants.dart';
@@ -35,33 +36,65 @@ class MyService extends StatelessWidget {
               ),
             ),
           ),
-          Constants.sizedBox(height: 6),
-          AnimatedContainer(
-            duration: const Duration(milliseconds: 600),
-            width: 450,
-            decoration: BoxDecoration(color: AppColors.bgColor2),
-            child: Column(
-              children: [
-                Image.asset(
-                  AppAssets.code,
-                  width: 50,
-                  height: 50,
-                  color: AppColors.themeColor,
-                ),
-                Constants.sizedBox(height: 30),
-                Text(
-                  "App Development",
-                  style: AppTextStyles.montserratStyle(),
-                ),
-                Constants.sizedBox(height: 12),
-                Text(
-                   " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-                    style: AppTextStyles.normalStyle(),
-                   textAlign: TextAlign.center,
-                ),
-              ],
-            ),
+          Constants.sizedBox(height: 40),
+          Row(
+            children: [
+              buildAnimatedContainer(
+                  title: "App Development", asset: AppAssets.code),
+              Constants.sizedBox(width: 18),
+              buildAnimatedContainer(
+                  title: "Figma Design", asset: AppAssets.brush),
+              Constants.sizedBox(width: 18),
+              buildAnimatedContainer(
+                  title: "WordPress", asset: AppAssets.analyst),
+            ],
           ),
+        ],
+      ),
+    );
+  }
+
+  AnimatedContainer buildAnimatedContainer(
+      {required String title, required String asset}) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 600),
+      width: 400,
+      height: 450,
+      alignment: Alignment.center,
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 24),
+      decoration: BoxDecoration(
+        color: AppColors.bgColor2,
+        borderRadius: BorderRadius.circular(30),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black54,
+            spreadRadius: 4.0,
+            blurRadius: 4.5,
+            offset: Offset(3.0, 4.5),
+          ),
+        ],
+      ),
+      child: Column(
+        children: [
+          Image.asset(
+            asset,
+            width: 50,
+            height: 50,
+            color: AppColors.themeColor,
+          ),
+          Constants.sizedBox(height: 30),
+          Text(
+            title,
+            style: AppTextStyles.montserratStyle(),
+          ),
+          Constants.sizedBox(height: 12),
+          Text(
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s...",
+            style: AppTextStyles.normalStyle(),
+            textAlign: TextAlign.center,
+          ),
+          Constants.sizedBox(height: 16),
+          AppButtons.buildMaterialButton(buttonName: "Read More", onTap: () {}),
         ],
       ),
     );
